0d4413b51bc6e013cab178754463348dc77252ab
Ugur Müslim <ugur@sistemkoin.com>
2020-12-14 14:04:36 +0300

    continue with index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,173 @@
 @section('content')
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
-        <div class="popular-games border-b border-gray-800 text-sm grid grid-cols-6 gap-12 pb-8">
+        <div class="popular-games border-b border-gray-800 text-sm grid grid-cols-1
+         md:grid-cols-2 lg:grid-cols-6  xl:grid-cols-6 gap-12 pb-8">
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="">
+                        <img class="hover:opacity-75 transition ease-in-out duration-150"
+                             src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png" alt="diablo">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                         style="bottom:-20px; right: -20px;">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8
+                transition ease-in-out duration-500">Diablo 3</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div>
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="">
@@ -19,31 +185,107 @@
                 <div class="text-gray-400 mt-1">Playstation 4</div>
             </div>
         </div>
-        <div class="flex my-10">
-            <div class="recently-reviewed w-3/4 mr-32">
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-reviewed w:full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
                     Recently Reviewed
                 </h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     <div class="bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
-                        <div class="relative inline-block">
+                        <div class="relative flex-none">
                             <a href="">
-                                <img class="hover:opacity-75 transition ease-in-out duration-150"
+                                <img class="w-32 hover:opacity-75 transition ease-in-out duration-150"
                                      src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png"
                                      alt="diablo">
                             </a>
-                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                            <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-900 rounded-full"
                                  style="bottom:-20px; right: -20px;">
                                 <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
                             </div>
                         </div>
+                        <div class="ml-12">
+                            <a href="" class="block text-lg font-semibold leading-tight hover:text-gray-400
+                transition ease-in-out duration-500">Diablo 3</a>
+                            <div class="text-gray-400 mt-1">Playstation 4</div>
+                            <p class="mt-2 text-gray-400 hidden lg:block">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aperiam
+                                assumenda deleniti enim esse, exercitationem facilis fugiat ipsum, modi mollitia nam
+                                nihil nobis officia possimus quasi quia vel velit voluptates voluptatum. Aliquam, amet
+                                assumenda atque, blanditiis dolorum enim illum maxime necessitatibus obcaecati officia,
+                                placeat quas quod reprehenderit repudiandae unde?
+                            </p>
+                        </div>
                     </div>
+                    <div class="bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="">
+                                <img class="w-32 hover:opacity-75 transition ease-in-out duration-150"
+                                     src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png"
+                                     alt="diablo">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-900 rounded-full"
+                                 style="bottom:-20px; right: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="" class="block text-lg font-semibold leading-tight hover:text-gray-400
+                transition ease-in-out duration-500">Diablo 3</a>
+                            <div class="text-gray-400 mt-1">Playstation 4</div>
+                            <p class="mt-2 text-gray-400 hidden lg:block">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aperiam
+                                assumenda deleniti enim esse, exercitationem facilis fugiat ipsum, modi mollitia nam
+                                nihil nobis officia possimus quasi quia vel velit voluptates voluptatum. Aliquam, amet
+                                assumenda atque, blanditiis dolorum enim illum maxime necessitatibus obcaecati officia,
+                                placeat quas quod reprehenderit repudiandae unde?
+                            </p>
+                        </div>
+                    </div>
+                    <div class="bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="">
+                                <img class="w-32 hover:opacity-75 transition ease-in-out duration-150"
+                                     src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png"
+                                     alt="diablo">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-12 h-12 bg-gray-900 rounded-full"
+                                 style="bottom:-20px; right: -20px;">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="" class="block text-lg font-semibold leading-tight hover:text-gray-400
+                transition ease-in-out duration-500">Diablo 3</a>
+                            <div class="text-gray-400 mt-1">Playstation 4</div>
+                            <p class="mt-2 text-gray-400 hidden lg:block">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aperiam
+                                assumenda deleniti enim esse, exercitationem facilis fugiat ipsum, modi mollitia nam
+                                nihil nobis officia possimus quasi quia vel velit voluptates voluptatum. Aliquam, amet
+                                assumenda atque, blanditiis dolorum enim illum maxime necessitatibus obcaecati officia,
+                                placeat quas quod reprehenderit repudiandae unde?
+                            </p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-            <div class="most-anticipated w-1/4">
+            <div class="most-anticipated w-full lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
                     Mostly Anticipated
                 </h2>
+                <div class="most-anticipated-container space-y-12 mt-8">
+                    <div class="game flex">
+                        <a href="">
+                            <img class="w-12 hover:opacity-75 transition ease-in-out duration-150"
+                                 src="https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png"
+                                 alt="diablo">
+                        </a>
+                        <div class="ml-4">
+                            <a href="" class="hover:text-gray-300">Diablo</a>
+                            <div class="text-gray-300 text-sm mt-1">Sept 16</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,34 +8,40 @@
     <link href="/css/app.css" rel="stylesheet">
 </head>
 <body class="bg-gray-900 text-white">
-    <header class="border-b border-gray-800">
-        <nav class="container mx-auto flex items-center justify-between px-4 py-3">
-            <div class="flex items-center">
-                <a href="">
-                    <img src="https://icons.veryicon.com/png/Game/Game/Diablo%20II.png"
-                         alt="game-image" class="w-24  flex-none">
-                </a>
-                <ul class="flex ml-16 space-x-8">
-                    <li><a href="" class="hover:text-gray-400">Games</a></li>
-                    <li><a href="" class="hover:text-gray-400">Reviews</a></li>
-                    <li><a href="" class="hover:text-gray-400">Coming Soon</a></li>
-                </ul>
-            </div>
-            <div class="flex items-center">
-                <div class="relative">
-                    <input type="text" class="text-sm rounded-full bg-gray-800 px-3 w-64 py-1 focus:outline-none
+<header class="border-b border-gray-800">
+    <nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-3">
+        <div class="flex  flex-col lg:flex-row items-center">
+            <a href="">
+                <img src="https://icons.veryicon.com/png/Game/Game/Diablo%20II.png"
+                     alt="game-image" class="w-24  flex-none">
+            </a>
+            <ul class="flex ml-0 lg:ml-16 space-x-8  mt-6 lg:mt-0">
+                <li><a href="" class="hover:text-gray-400">Games</a></li>
+                <li><a href="" class="hover:text-gray-400">Reviews</a></li>
+                <li><a href="" class="hover:text-gray-400">Coming Soon</a></li>
+            </ul>
+        </div>
+        <div class="flex items-center mt-6 lg:mt-0">
+            <div class="relative">
+                <input type="text" class="text-sm rounded-full bg-gray-800 px-3 w-64 py-1 focus:outline-none
                     focus:shadow-outline-black pl-8" placeholder="Search...">
-                    <div class="absolute top-0 flex items-center h-full ml-2">
-                        <svg class="text-gray-400 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                    </div>
+                <div class="absolute top-0 flex items-center h-full ml-2">
+                    <svg class="text-gray-400 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                    </svg>
                 </div>
-                <div class="ml-6">
-                    <a href=""><img src="https://lh3.googleusercontent.com/ogw/ADGmqu_HqHHv95kQQ-RBR2tJBCbvn1c70RMriQjheOKLtA=s32-c-mo" class="rounded-full w-8" alt=""></a></div>
             </div>
+            <div class="ml-6">
+                <a href=""><img
+                        src="https://lh3.googleusercontent.com/ogw/ADGmqu_HqHHv95kQQ-RBR2tJBCbvn1c70RMriQjheOKLtA=s32-c-mo"
+                        class="rounded-full w-8" alt=""></a></div>
+        </div>
 
-        </nav>
+    </nav>
 
-    </header>
+</header>
 <main class="py-8">
     @yield('content')
 </main>
